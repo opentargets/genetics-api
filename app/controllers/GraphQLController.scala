c73e7a01dbf73424e83f51ce1a53b1ac3e005686
Miguel C <mkarmona@gmail.com>
2018-10-07 17:45:59 +0100

    fix: dense region checker works as expected again
    tsv file wasn't loaded from the resource folder when deployed to production as file wasn't found anymore

--- app/controllers/GraphQLController.scala
+++ app/controllers/GraphQLController.scala
@@ -13,7 +13,6 @@ import sangria.execution._
 import sangria.parser.{QueryParser, SyntaxError}
 import sangria.marshalling.playJson._
 import models.Violations._
-import models.Entities._
 
 import scala.util.{Failure, Success}
 
