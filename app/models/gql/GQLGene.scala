b18a8aa826a38dd4bea75b07467baba494612f33
Jarrod Baker <jarrodbaker@ebi.ac.uk>
2020-06-25 15:36:16 +0200

    GraphQL: extracting traits into new package

---
+++ app/models/gql/GQLGene.scala
@@ -0,0 +1,31 @@
+package models.gql
+
+import models.Backend
+import models.entities.DNA.Gene
+import sangria.execution.deferred.{Fetcher, FetcherConfig, HasId}
+import sangria.macros.derive.deriveObjectType
+import sangria.schema.{Field, FloatType, ObjectType, fields}
+
+trait GQLGene {
+  implicit val geneHasId: HasId[Gene, String] = HasId[Gene, String](_.id)
+
+  val genesFetcher: Fetcher[Backend, Gene, Gene, String] = Fetcher(
+    config = FetcherConfig.maxBatchSize(100),
+    fetch = (ctx: Backend, geneIds: Seq[String]) => {
+      ctx.getGenes(geneIds)
+    })
+
+  implicit val gene: ObjectType[Backend, Gene] = deriveObjectType[Backend, Gene]()
+
+  val scoredGene: ObjectType[Backend, (String, Double)] = ObjectType(
+    "ScoredGene",
+    "This object link a Gene with a score",
+    fields[Backend, (String, Double)](
+      Field("gene", gene, Some("Gene Info"), resolve = rsl => genesFetcher.defer(rsl.value._1)),
+      Field(
+        "score",
+        FloatType,
+        Some("Score a Float number between [0. .. 1.]"),
+        resolve = _.value._2)))
+
+}
---
+++ app/models/gql/GQLStudy.scala
@@ -0,0 +1,32 @@
+package models.gql
+
+import models.Backend
+import models.entities.Entities.Study
+import sangria.execution.deferred.{Fetcher, FetcherConfig, HasId}
+import sangria.macros.derive.{AddFields, DocumentField, deriveObjectType}
+import sangria.schema.{Field, LongType, ObjectType}
+
+trait GQLStudy {
+  implicit val studyHasId: HasId[Study, String] = HasId[Study, String](_.studyId)
+
+  val studiesFetcher: Fetcher[Backend, Study, Study, String] = Fetcher(
+    config = FetcherConfig.maxBatchSize(100),
+    fetch = (ctx: Backend, stids: Seq[String]) => {
+      ctx.getStudies(stids)
+    })
+
+  val study: ObjectType[Backend, Study] = deriveObjectType[Backend, Study](
+    DocumentField("traitReported", "Trait Label as reported on the publication"),
+    DocumentField("traitEfos", "A list of curated efo codes"),
+    DocumentField("pubId", "PubMed ID for the corresponding publication"),
+    DocumentField("pubDate", "Publication Date as YYYY-MM-DD"),
+    DocumentField("pubJournal", "Publication Journal name"),
+    DocumentField("hasSumstats", "Contains summary statistical information"),
+    AddFields(
+      Field(
+        "nTotal",
+        LongType,
+        Some("n total cases (n initial + n replication)"),
+        resolve = r => r.value.nInitial.getOrElse(0L) + r.value.nReplication.getOrElse(0L))))
+
+}
